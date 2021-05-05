590433cdd1434c57f5ed5235edefe30a40d1a2a2
csagastegui59 <csagastegui59@gmail.com>
2021-05-05 14:04:24 -0500

    add readme file

--- lilab-test/src/components/footer.js
+++ lilab-test/src/components/footer.js
@@ -4,7 +4,7 @@ import logo from "./lilab-logo.png";
 export default function Footer(){
   return (
     <footer>
-      <div className="flex justify-between items-center bg-white py-10 px-4 shadow-2xl">
+      <div className="flex justify-between items-center bg-white py-10 px-4 shadow-2xl fixed bottom-0 w-full">
         <div className="opacity-50">©2021 Lilab, Inc. All rights reserved</div>
         <img src={logo} alt="Logo" className="w-28"/>
       </div>
